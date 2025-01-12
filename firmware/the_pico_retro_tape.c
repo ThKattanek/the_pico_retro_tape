d1775533663fa98aa5c1fb0f4c1cd18d6df42271
Thorsten Kattanek <thorsten.kattanek@gmx.de>
2025-01-12 21:04:40 +0100

    firmware: add play_button and SENSE and READ

--- firmware/the_pico_retro_tape.c
+++ firmware/the_pico_retro_tape.c
@@ -1,13 +1,48 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+#define PLAY_BUTTON_GPIO        16
+
+#define C64_TAPE_READ_GPIO      2
+#define C64_TAPE_SENSE_GPIO     3
+
+void init_tape_ports_c64()
+{
+    gpio_init(C64_TAPE_READ_GPIO);
+    gpio_init(C64_TAPE_SENSE_GPIO);
+
+    gpio_set_dir(C64_TAPE_READ_GPIO, true);
+    gpio_set_dir(C64_TAPE_SENSE_GPIO, true);
+}
 
 int main()
 {
     stdio_init_all();
 
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+    //  PlayButton is Input an set pull down
+    gpio_init(PLAY_BUTTON_GPIO);
+    gpio_set_dir(PLAY_BUTTON_GPIO, false);
+    gpio_set_pulls(PLAY_BUTTON_GPIO, false, true);
+
+    init_tape_ports_c64();
+
+    gpio_put(C64_TAPE_SENSE_GPIO, true);
+
+bool play_button_new_state = false;
+bool play_button_old_state = false;
+
+    while (true) 
+    {
+        play_button_new_state = gpio_get(PLAY_BUTTON_GPIO);
+        
+        if(play_button_new_state == true && play_button_old_state == false)
+        {
+            printf("Play Button is pressed!\n");
+            gpio_put(C64_TAPE_SENSE_GPIO, false);
+        }
+
+        play_button_old_state = play_button_new_state;
+
+        sleep_ms(100);
     }
 }
